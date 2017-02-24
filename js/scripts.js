c1eb0d80ddabb13276f10dc9cfd2a4260009599f
Megan Warnock <megandwarnock@gmail.com>
2017-02-24 11:11:44 -0800

    Add branching backend logic for numbers divisible by 3, 5, and 10.

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,16 @@
 //back end logic
 
-
-
+var divisible = function(number) {
+  if (number % 3 === 0) {
+    number = "ping";
+    console.log(number);
+  } else if (number % 5 === 0) {
+    number = "pong";
+    console.log(number);
+  } else if (number % 15 === 0){
+    number = "ping-pong";
+  }
+};
 
 
 //front end logic
@@ -9,8 +18,7 @@ $(function(){
   $("#pingPong").submit(function(){
     event.preventDefault();
     var inputNum = $("input#numInput").val();
-    console.log(inputNum);
-
+    var result = divisible(inputNum);
     $("#output").text(inputNum);
   });
 });
